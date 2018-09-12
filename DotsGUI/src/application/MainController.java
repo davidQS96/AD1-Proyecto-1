984406cf350702060f32c9cfa2d318986e4a87c5
davidQS96 <david.qs@hotmail.com>
2018-09-11 21:10:36 -0600

    Commit con cambios en apariencia del GameGUI.

---
+++ DotsGUI/src/application/MainController.java
@@ -0,0 +1,15 @@
+package application;
+
+import javafx.event.ActionEvent;
+
+public class MainController {
+
+	public void processNumbers(ActionEvent event) {
+		
+	}
+	
+	public void processOperator(ActionEvent event) {
+			
+		}
+	
+}
---
+++ DotsGUI/src/application/Model.java
@@ -0,0 +1,12 @@
+package application;
+
+public class Model {
+	public float calculate(long n1, long n2, String operator) {
+		
+		
+		
+		return 0;
+		
+	}
+
+}
