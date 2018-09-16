a011a96670cb395224b4004c47eaece4e70dc5c8
jonDorito <jonDorito@users.noreply.github.com>
2018-09-15 20:05:25 -0600

    Prueba de implementacion del Servidor

---
+++ ServerANDclient/PruebaDT2/src/pruebadt2/Client.java
@@ -0,0 +1,20 @@
+
+package pruebadt2;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+
+public class Client {
+    
+    private static int PORT = 65530;
+    private Socket socket;
+    private BufferedReader in;
+    private PrintWriter out;
+    
+    
+}
---
+++ ServerANDclient/PruebaDT2/src/pruebadt2/ListDots.java
@@ -0,0 +1,22 @@
+package pruebadt2;
+
+public class ListDots {
+	List<Dot> list = new List<Dot>();
+	
+	public void addLast(Dot dot) {
+		list.addLast(dot);
+	}
+	
+	/*
+	 * Funcion que muestra en al consola todos los puntos que estan en la matrizs 
+	 * se separan por una nueva linea
+	 */
+	public void printDots() {
+		Nodo<Dot> temp = list.first;
+		while (temp.next != null) {
+			System.out.println("X = "+ temp.data.x+ ", Y = "+ temp.data.Y+", owener = "+ temp.data.owner+ "; " );
+			temp = temp.next;
+		}
+		System.out.println("X = "+ temp.data.x+ ", Y = "+ temp.data.Y+", owener = "+ temp.data.owner+"." );
+	}	
+}
