f7e541e7694732c265a96a103038a5aa7e7b86b7
davidQS96 <david.qs@hotmail.com>
2018-09-18 16:17:57 -0600

    Commit de interfaz de menu y conexion.
    Se comenzaron las ventanas para menu principal, y menu de opciones de
    conexion

--- DotsGUI/src/application/GameController.java
+++ DotsGUI/src/application/GameController.java
@@ -1,9 +1,7 @@
 package application;
 
-import java.util.ArrayList;
-
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
@@ -14,36 +12,65 @@ public class GameController {
 	
 	@FXML
 	private AnchorPane gameArea; //Pane en donde se lleva a cabo el juego
-	private Integer rowNum = 7; //Numero de filas de puntos
-	private Integer colNum = 3; //Numero de columnas de puntos
+	private Integer rowNum = 5; //Numero de filas de puntos
+	private Integer colNum = 8; //Numero de columnas de puntos
+	private Integer[] gameAreaSize = {784, 441}; //Dimensiones del area de juego
+	private Integer[] distDots = new Integer[2];
+	private Integer dotRadius = 4;
+	private Integer lAllow = dotRadius + 8;
+	private Integer hAllow =  dotRadius + 8;
+	private boolean isDrawingLine = false;
 	
-	//private ArrayList<ArrayList<Integer[]>> posMatrix = new ArrayList<ArrayList<Integer[]>>() ;//Aqui va matriz con posiciones (noc si va con listas creadas o se pueden usar las de java)
-
-	public void initialize() {
-		for(int row = 0; row <= rowNum; row++ ) {
-			//posMatrix.add(new ArrayList<Integer>());
+	private Integer[] posInitLine = new Integer[2];
+	
+	public void initialize() { //Metodo que se llama cuando se abre la ventana de juego
+		distDots[0] = Math.round(gameAreaSize[0]/(colNum - 1));
+		distDots[1] = Math.round(gameAreaSize[1]/(rowNum - 1));
+		
+		for(int row = 0; row < rowNum; row++ ) {
 			
-			for(int column = 0; column <= colNum; column++ ) {
-				//Integer[] temp_array = new Integer[2];
+			for(int column = 0; column < colNum; column++ ) {
 				
+				//Parte para crear dots
 				double radius = 4;
-				double posX = row * 100;
-				double posY = 20 + column * 100;
+				double posX = distDots[0] * column;
+				double posY = distDots[1] * row;
 				Paint fillColor = Color.BLACK;
 				
-				//posMatrix.get();
-				
 				Circle dot_i = new Circle(posX, posY, radius, fillColor);
-				gameArea.getChildren().add(dot_i);	
+				//Button button_i = new Button();
+				//button_i.prefHeight(radius + 4);
+				//button_i.prefHeight(radius + 4);
+				gameArea.getChildren().add(dot_i);
 				
 			}
 		}		
 	}
-	
-	public void beginLine(MouseEvent event) {
-		System.out.println(event.getX());
-		System.out.println(event.getY());	
+
+	@FXML
+    private void beginLine(MouseEvent event){
+		double posX = event.getX();
+		double posY = event.getY();
 		
-	}
+		//Est condicion permite detectar si se hizo clic dentro de una zona permitida alrededor de un punto para comenzar una linea.
+		if((posX + distDots[0] / 2) % distDots[0] > (distDots[0] - lAllow)/2  && (posX + distDots[0] / 2) % distDots[0] < (distDots[0] + lAllow)/2) {
+			if((posY + distDots[1] / 2) % distDots[1] > (distDots[1] - hAllow)/2  && (posY + distDots[1] / 2) % distDots[1] < (distDots[1] + hAllow)/2) {
+				 if(!isDrawingLine) {
+					 isDrawingLine = true;
+					 System.out.println("Hola");
+					 						 
+					 
+				 }else {
+					 isDrawingLine = false;
+					 //Line tempLine = new Line(double startX, double startY, double endX, double endY);
+				 }
+			}
+		}
+		
+    }
+	
+	
+
+	
 	
 }
---
+++ DotsGUI/src/application/MainMenuController.java
@@ -0,0 +1,36 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class MainMenuController {
+	@FXML
+	private Button playButton;
+	
+	@FXML
+	private Button connOptButton;
+	
+	public void openGameScreen(ActionEvent event){
+		try {
+			Stage primaryStage = new Stage();
+			Parent root = FXMLLoader.load(getClass().getResource("/application/GameScreen.fxml"));
+			Scene scene = new Scene(root);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+						
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void openConnOptScreen(){
+		
+	}
+
+}
