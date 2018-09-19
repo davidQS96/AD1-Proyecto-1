2474d6ea7c28e6fe8b12fb9595ee8330ef955afb
davidQS96 <david.qs@hotmail.com>
2018-09-19 11:34:24 -0600

    2 commit del miercoles

--- DotsGUI/src/application/MainMenuController.java
+++ DotsGUI/src/application/MainMenuController.java
@@ -24,7 +24,7 @@ public class MainMenuController {
 						
 			primaryStage.setScene(scene);
 			primaryStage.show();
-		} catch(Exception e) {
+		} catch(Exception e)  {
 			e.printStackTrace();
 		}
 	}
