f17aabebcab8eff9bd11574570084160e0ba5d6d
JATS14 <42524204+JATS14@users.noreply.github.com>
2018-10-12 18:10:05 -0600

    cambios de nuevo

--- SemiFuntionalNetbeans Version/DOTS/src/dots/Player.java
+++ SemiFuntionalNetbeans Version/DOTS/src/dots/Player.java
@@ -30,8 +30,8 @@ public class Player {
         this.beenAssigned = beenAssigned;
     }
         
-        public void setPoints(int points) {
-            this.points = points;
+        public void setPoints(int points2) {
+            this.points = points+points2;
 	}
 
         public void setOpponent(Player opponent) {
